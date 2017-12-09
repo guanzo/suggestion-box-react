19560003ea775369b61af20b8544da4f92c1b283
eric guan <guanzo91@gmail.com>
2017-12-08 19:27:45 -0800

    added tests to user-util

--- shared/user-util.test.js
+++ shared/user-util.test.js
@@ -1,5 +1,18 @@
-import userUtil from './user-util'
+const chai = require('chai')
+const userUtil = require('./user-util')
+const expect = chai.expect
+const moment = require('moment')
 
 describe('user-util',()=>{
-	expect(userUtil.MIN_MINUTES_BETWEEN_POSTS).toEqual(1440)
+	it('has MIN_MINUTES_BETWEEN_POSTS equal to one day',()=>{
+		expect(userUtil.MIN_MINUTES_BETWEEN_POSTS).to.equal(1440)
+	})
+	it('returns false if 24 hours hasn\'t passed',()=>{
+		let lastSuggestDate = moment().subtract(1,'hours')
+		expect(userUtil.isAllowedToSuggest(lastSuggestDate)).to.be.false
+	})
+	it('returns true if 24 hours has passed',()=>{
+		let lastSuggestDate = moment().subtract(25,'hours')
+		expect(userUtil.isAllowedToSuggest(lastSuggestDate)).to.be.true
+	})
 })
